c939383a20570dca188a66165ccad5cf735bf07c
txj-xyz <7461476+txj-xyz@users.noreply.github.com>
2025-01-07 10:45:24 -0700

    fix: use ts_ls instead of deprecated tsserver

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- LSP Servers to Enable
-local servers = { "html", "cssls", "bashls", "gopls" }
+local servers = { "html", "cssls", "bashls", "gopls", "ts_ls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
